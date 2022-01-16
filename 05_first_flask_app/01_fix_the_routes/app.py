8248dd14638a2dd20cb295943d36ae8c4cc3be7e
picardcharlie <picardcharlie@gmail.com>
2022-01-16 16:00:18 -0600

    flask 05 lab

--- 05_first_flask_app/01_fix_the_routes/app.py
+++ 05_first_flask_app/01_fix_the_routes/app.py
@@ -5,15 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    page = "Welcome to Emily's Dog Costumes! Ohh no."
+    page = "Welcome to Emily's Dog Costumes!."
+    return page
 
-
-@app.route('services')
+@app.route('/services')
 def services():
     page = "I offer custom made costumes for your precious canine companion, "\
         "and a free in-home consultation, to get the measurements."
+    return page
 
-
-@app.route('/costumes/costume')
+@app.route('/costumes/<costume>')
 def costumes(costume):
     page = f"Check out this {costume} costume!"
+    return page
